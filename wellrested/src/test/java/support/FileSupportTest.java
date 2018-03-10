4b1af3e31d078070adefa3d71732db1a017c1f1e
regulardave84 <chrisdavies5@hotmail.com>
2018-03-10 23:28:28 +0000

    Added new test for multiple replaces in a file with a single line.

--- wellrested/src/test/java/support/FileSupportTest.java
+++ wellrested/src/test/java/support/FileSupportTest.java
@@ -9,18 +9,22 @@ import static org.hamcrest.core.Is.is;
 
 public class FileSupportTest {
     private static final String REPLACEMENT_TEXT = "text_replaced!";
-    private static final String FILE_WITH_SINGLE_LINE_PATH =
-            "TestData/FileSupportTestData/replacement_for_file_with_single_line.json";
+    private static final String TEST_DATA_FILE_SUPPORT_PATH = "TestData/FileSupportTestData/";
+    private static final String REPLACEMENT_FOR_FILE_WITH_SINGLE_LINE_PATH =
+            TEST_DATA_FILE_SUPPORT_PATH + "replacement_for_file_with_single_line.json";
+    private static final String MULTIPLE_REPLACEMENTS_FILE_WITH_SINGLE_LINE_PATH =
+            TEST_DATA_FILE_SUPPORT_PATH + "multiple_replacements_for_file_with_single_line.json";
     private static final String TEXT_TO_REPLACE1 = "%REPLACE%";
-    private static final String FILE_WITH_MULTIPLE_LINES_PATH =
-            "TestData/FileSupportTestData/replacement_for_file_with_multiple_lines.json";
+    private static final String REPLACEMENT_FOR_FILE_WITH_MULTIPLE_LINES_PATH =
+            TEST_DATA_FILE_SUPPORT_PATH + "/replacement_for_file_with_multiple_lines.json";
 
     FileSupport fs = new FileSupport();
 
     @Test
     public void testGetFileAsStringWithReplacementForFileWithASingleLine() throws IOException {
         String fileWithReplacementAsString =
-                fs.getFileAsStringWithReplacement(FILE_WITH_SINGLE_LINE_PATH, TEXT_TO_REPLACE1, REPLACEMENT_TEXT);
+                fs.getFileAsStringWithReplacement(REPLACEMENT_FOR_FILE_WITH_SINGLE_LINE_PATH,
+                        TEXT_TO_REPLACE1, REPLACEMENT_TEXT);
         String expectedString = String.format("{ \"id\": \"%s\" }", REPLACEMENT_TEXT);
         assertThat(fileWithReplacementAsString, is(expectedString));
     }
@@ -28,8 +32,20 @@ public class FileSupportTest {
     @Test
     public void testGetFileAsStringWithReplacementForFileWithMultipleLines() throws IOException {
         String fileWithReplacementAsString =
-                fs.getFileAsStringWithReplacement(FILE_WITH_MULTIPLE_LINES_PATH, TEXT_TO_REPLACE1, REPLACEMENT_TEXT);
+                fs.getFileAsStringWithReplacement(REPLACEMENT_FOR_FILE_WITH_MULTIPLE_LINES_PATH,
+                        TEXT_TO_REPLACE1, REPLACEMENT_TEXT);
         String expectedString = String.format("{\n  \"id\": \"%s\"\n}", REPLACEMENT_TEXT);
+        assertThat(fileWithReplacementAsString, is(expectedString));
+    }
+
+    @Test
+    public void testGetFileAsStringWithReplacementForMultipleReplacementsInAFileWithASingleLine() throws IOException {
+        String fileWithReplacementAsString =
+                fs.getFileAsStringWithReplacement(MULTIPLE_REPLACEMENTS_FILE_WITH_SINGLE_LINE_PATH,
+                                                  TEXT_TO_REPLACE1,
+                                                  REPLACEMENT_TEXT);
+        String expectedString = String.format("{ \"id\": \"%s\", \"another_id\": \"%s\" }",
+                REPLACEMENT_TEXT, REPLACEMENT_TEXT);
         assertThat(fileWithReplacementAsString, is(expectedString));
     }
 }
